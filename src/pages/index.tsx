e02ea7d197168d9ec8a6b7a94d86e2bec84ad89c
danielduarte-baird <piduartebaird@gmail.com>
2022-03-15 12:55:09 -0400

    initial commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,179 +1,177 @@
 import * as React from "react"
-
+import "../../public/css/styles.css"
+import NaviButton from "../../public/static/NaviButton";
+import Navi from "../../public/static/Navi";
+import Post from "../../public/static/Post";
+import Leader from "../../public/static/Leader";
+import PeopleBox from "../../public/static/PeopleBox";
+import "../../public/static/Normie";
+import PeopleCarousel from "../../public/static/PeopleCarousel";
+import TextBox from "../../public/static/TextBox";
+import { Normie, select } from "../../public/static/Normie";
+import { SpecialZoomLevel, Viewer } from '@react-pdf-viewer/core';
+import { Worker } from '@react-pdf-viewer/core';
+// Import the styles
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import { Document, Page } from 'react-pdf';
 // styles
 const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
+  color: "rgb(100, 100, 100)",
+};
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "TypeScript Documentation",
-  url: "https://www.gatsbyjs.com/docs/how-to/custom-configuration/typescript/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative" as "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
 
 // data
 const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
+  
 ]
 
 // markup
 const IndexPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        🎉🎉🎉
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.tsx</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=ts-docs&utm_campaign=minimal-starter-ts`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter-ts`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
+
+  <main>
+      <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"></link>
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossOrigin="anonymous"></link>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" crossOrigin="anonymous"></link>
+      <script src="https://kit.fontawesome.com/64919bd2ce.js" crossOrigin="anonymous"></script>
+      <title>wheelerrobotics!</title>
+      <Navi>
+        <NaviButton title={"Home"} refer={"home"} img={"fa fa-home"} icn={""}></NaviButton>
+        <NaviButton title={"About Us"} refer={"about"} img={"fa fa-users"} icn={""}></NaviButton>
+        <NaviButton title={"Our Work"} refer={"work"} img={"fa fa-comment"} icn={""}></NaviButton>
+        <NaviButton title={"Give"} refer={"give"} img={"fa fa-heart"} icn={""}></NaviButton>
+      </Navi>
+      <div id="pages">
+        <div id="home">
+          <div className="page_area">
+            <div className="title_area">
+              <h1>Logo goes here!</h1>
+            </div>
+            <div className="posts_area">
+              <Post title={"Initial Commit"} subtitle={"How we became a team"} date={"11/19/20"} img={"https://i.vimeocdn.com/video/1100868839-c27cae7588087e5630afd15af9590d256842a20bf742b4f0cf793b8e64ae3ac4-d?mw=1000&mh=563&q=70"}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                <br></br>
+                Officiis veniam laudantium tempore magni sunt aliquam neque non velit saepe? Iure, similique qui fugiat inventore quia corporis deleniti minima natus consequuntur.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                <br></br>
+                Officiis veniam laudantium tempore magni sunt aliquam neque non velit saepe? Iure, similique qui fugiat inventore quia corporis deleniti minima natus consequuntur.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                <br></br>
+                Officiis veniam laudantium tempore magni sunt aliquam neque non velit saepe? Iure, similique qui fugiat inventore quia corporis deleniti minima natus consequuntur.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                <br></br>
+                Officiis veniam laudantium tempore magni sunt aliquam neque non velit saepe? Iure, similique qui fugiat inventore quia corporis deleniti minima natus consequuntur.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                <br></br>
+                Officiis veniam laudantium tempore magni sunt aliquam neque non velit saepe? Iure, similique qui fugiat inventore quia corporis deleniti minima natus consequuntur.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                <br></br>
+                Officiis veniam laudantium tempore magni sunt aliquam neque non velit saepe? Iure, similique qui fugiat inventore quia corporis deleniti minima natus consequuntur.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                <br></br>
+                Officiis veniam laudantium tempore magni sunt aliquam neque non velit saepe? Iure, similique qui fugiat inventore quia corporis deleniti minima natus consequuntur.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                <br></br>
+                Officiis veniam laudantium tempore magni sunt aliquam neque non velit saepe? Iure, similique qui fugiat inventore quia corporis deleniti minima natus consequuntur.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                <br></br>
+                Officiis veniam laudantium tempore magni sunt aliquam neque non velit saepe? Iure, similique qui fugiat inventore quia corporis deleniti minima natus consequuntur.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                <br></br>
+                Officiis veniam laudantium tempore magni sunt aliquam neque non velit saepe? Iure, similique qui fugiat inventore quia corporis deleniti minima natus consequuntur.
+
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                <br></br>
+                Officiis veniam laudantium tempore magni sunt aliquam neque non velit saepe? Iure, similique qui fugiat inventore quia corporis deleniti minima natus consequuntur.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                <br></br>
+                Officiis veniam laudantium tempore magni sunt aliquam neque non velit saepe? Iure, similique qui fugiat inventore quia corporis deleniti minima natus consequuntur.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                <br></br>
+                Officiis veniam laudantium tempore magni sunt aliquam neque non velit saepe? Iure, similique qui fugiat inventore quia corporis deleniti minima natus consequuntur.
+              </Post>
+              <Post title={"Initial Commit"} subtitle={"How we became a team"} date={"11/19/20"} img={"https://iso.500px.com/wp-content/uploads/2014/06/W4A2827-1-3000x2000.jpg"}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod in, neque consequuntur animi recusandae atque asperiores beatae saepe nemo quam! Ipsam sit consequatur veniam amet minus fuga quam accusamus eos.
+              </Post>
+              <Post title={"Initial Commit"} subtitle={"How we became a team"} date={"11/19/20"} img={"https://iso.500px.com/wp-content/uploads/2014/06/W4A2827-1-3000x2000.jpg"}>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam, asperiores dolores? Iste facere facilis optio maiores amet nostrum corrupti aut minus, dicta explicabo, ex ratione a enim ab. Nihil, dolor.
+              </Post>
+            </div>
+          </div>
+        </div>
+        <div id="about" hidden>
+          <div className="title_area">
+            <h1>About Us</h1>
+          </div>
+          <div className="people_area">
+
+          <div className="leader_area">
+            <PeopleBox title={"Leadership"}>
+              <Leader name={"William Kopans"} grad_year={"2022"} email={"william.kopans@wheelergmail.org"} bio={"He's a bird, he's a plane, he's will kopans!"} img={"https://challengepost-s3-challengepost.netdna-ssl.com/photos/production/user_photos/000/814/873/datas/xlarge.png"}></Leader>
+              <Leader name={"Mayte Segura"} grad_year={"2022"} email={"mayte.segura@wheelergmail.org"} bio={"SHe's a bird, she's a plane, she's mayte segura!"} img={"https://challengepost-s3-challengepost.netdna-ssl.com/photos/production/user_photos/000/814/873/datas/xlarge.png"}></Leader>
+            </PeopleBox>
+            </div>
+            <TextBox title={"About Us"}> We are the electric quahogs! yay! </TextBox>
+              <PeopleCarousel>
+                <Normie name={"William Kopans"} grad_year={"2022"} email={"william.kopans@wheelergmail.org"} bio={"He's a bird, he's a plane, he's will kopans!"} img={"https://challengepost-s3-challengepost.netdna-ssl.com/photos/production/user_photos/000/814/873/datas/xlarge.png"}></Normie>
+                <Normie name={"William Kopans"} grad_year={"2022"} email={"william.kopans@wheelergmail.org"} bio={"He's a bird, he's a plane, he's will kopans!"} img={"https://challengepost-s3-challengepost.netdna-ssl.com/photos/production/user_photos/000/814/873/datas/xlarge.png"}></Normie>
+                <Normie name={"William Kopans"} grad_year={"2022"} email={"william.kopans@wheelergmail.org"} bio={"He's a bird, he's a plane, he's will kopans!"} img={"https://challengepost-s3-challengepost.netdna-ssl.com/photos/production/user_photos/000/814/873/datas/xlarge.png"}></Normie>
+                <Normie name={"William Kopans"} grad_year={"2022"} email={"william.kopans@wheelergmail.org"} bio={"He's a bird, he's a plane, he's will kopans!"} img={"https://challengepost-s3-challengepost.netdna-ssl.com/photos/production/user_photos/000/814/873/datas/xlarge.png"}></Normie>
+                <Normie name={"William Kopans"} grad_year={"2022"} email={"william.kopans@wheelergmail.org"} bio={"He's a bird, he's a plane, he's will kopans!"} img={"https://challengepost-s3-challengepost.netdna-ssl.com/photos/production/user_photos/000/814/873/datas/xlarge.png"}></Normie>
+                <Normie name={"William Kopans"} grad_year={"2022"} email={"william.kopans@wheelergmail.org"} bio={"He's a bird, he's a plane, he's will kopans!"} img={"https://challengepost-s3-challengepost.netdna-ssl.com/photos/production/user_photos/000/814/873/datas/xlarge.png"}></Normie>
+                <Normie name={"William Kopans"} grad_year={"2022"} email={"william.kopans@wheelergmail.org"} bio={"He's a bird, he's a plane, he's will kopans!"} img={"https://challengepost-s3-challengepost.netdna-ssl.com/photos/production/user_photos/000/814/873/datas/xlarge.png"}></Normie>
+                <Normie name={"William Kopans"} grad_year={"2022"} email={"william.kopans@wheelergmail.org"} bio={"He's a bird, he's a plane, he's will kopans!"} img={"https://challengepost-s3-challengepost.netdna-ssl.com/photos/production/user_photos/000/814/873/datas/xlarge.png"}></Normie>
+                <Normie name={"William Kopans"} grad_year={"2022"} email={"william.kopans@wheelergmail.org"} bio={"He's a bird, he's a plane, he's will kopans!"} img={"https://challengepost-s3-challengepost.netdna-ssl.com/photos/production/user_photos/000/814/873/datas/xlarge.png"}></Normie>
+              </PeopleCarousel>
+          </div>
+        <div className="people_area">
+          man, theres so many thing that are interesting about us :)
+        </div>
+      </div>
+      <div id="give" hidden>
+        <div className="title_area">
+            <h1>How <i>You</i> Can Help</h1>
+          </div>
+        <div className="give_div">
+          this is where they learn how to "contribute" to our team, whatever that means
+        </div>
+      </div>
+      <div id="feedback">
+        <div style={{ marginTop: "10%" }}>
+          <p>suggestions go here :)</p>
+          <input id="suggestions" placeholder="this doesnt actually do anything"></input>
+        </div>
+      </div>
+      <div id="work">
+        <div className="title_area">
+          <h1>What We Have Done</h1>
+        </div>
+        <div id="page_area">
+          <TextBox title={"Our Portfolio"}>
+            <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.13.216/build/pdf.worker.min.js">
+            <div
+                style={{
+                    border: '1px solid rgba(0, 0, 0, 0.3)',
+                    height: '700px',
+                    width: "40%",
+                }}
+            >
+              <Viewer fileUrl="Engineering Portfolio 2021-2022.pdf" defaultScale={SpecialZoomLevel.PageFit} />
+              </div>
+            </Worker>   
+          </TextBox>
+        </div>
+      </div>
+    </div><div className="footer">
+        this can be where all the sponsors and other footer stuff goes
+      </div>
+      
+
+
+
     </main>
-  )
+
+  );
 }
 
 export default IndexPage
