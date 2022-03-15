b0db6d065bdf75509931f208a0b79e648907c227
danielduarte-baird <piduartebaird@gmail.com>
2022-03-15 17:41:16 -0400

    Update 404.tsx

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import { Link } from "gatsby"
 
 // styles
-const pageStyles = {
+/* const pageStyles = {
   color: "#232129",
   padding: "96px",
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
@@ -23,30 +23,14 @@ const codeStyles = {
   fontSize: "1.25rem",
   borderRadius: 4,
 }
-
+ */
 // markup
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
+    <main>
+      <title>Page Not Found</title>
+        <p>If this inconvinienced you, please send <a>william.kopans@wheelergmail.org</a> a very angry email</p>
+        <Link to="/">Go home</Link>
     </main>
   )
 }
