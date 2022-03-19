87f330ad015efcbb4d361bc64e0e9dac85f54fa2
danielduarte-baird <piduartebaird@gmail.com>
2022-03-18 23:45:12 -0400

    fix new window build err

--- src/segments/SideMenuSegment.tsx
+++ src/segments/SideMenuSegment.tsx
@@ -1,9 +1,10 @@
 import * as React from "react"
 import SideBar from "../components/SideBar"
 import SideBarButton from "../components/SideBarButton"
+import isBrowser from "../helpers/isBrowser"
 
 function SideMenuSegment() {
-        return <SideBar title={window ? (window.innerWidth > 600) ? "Loving The Electic Quahogs?" : "" : ""}>
+        return <SideBar title={isBrowser() ? (window.innerWidth > 600) ? "Loving The Electic Quahogs?" : "" : ""}>
                     <SideBarButton title={"Latest News!"} redirect={"news"}></SideBarButton>
                     <SideBarButton title={"Our Github!"} redirect={"https://github.com/wheelerrobotics"}></SideBarButton>
                     <SideBarButton title={"Sign Up!"} redirect={"https://docs.google.com/forms/d/e/1FAIpQLSdnxH2JlEnOVB2kW3c0EjCPh8TfeSSj3Itm8qfXFJKTSbQPfA/viewform"}></SideBarButton>
