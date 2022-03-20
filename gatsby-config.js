04f46dd7a1bf71b4ea3ff56cfd3d9b3a2a0e2710
danielduarte-baird <piduartebaird@gmail.com>
2022-03-19 21:52:35 -0400

    add sitemap plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,54 @@ module.exports = {
         keywords: ["252", "electric", "quahogs", "wheeler", "robotics", "first", "tech", "challenge", "FTC", "team", "highschool", "upperschool", "school"],
     },
     plugins: [
-      `gatsby-plugin-gatsby-cloud`,
+      `gatsby-plugin-gatsby-cloud`,{
+        resolve: "gatsby-plugin-sitemap",
+        options: {
+          query: `
+          {
+            allSitePage {
+              nodes {
+                path
+              }
+            }
+            allWpContentNode(filter: {nodeType: {in: ["Post", "Page"]}}) {
+              nodes {
+                ... on WpPost {
+                  uri
+                  modifiedGmt
+                }
+                ... on WpPage {
+                  uri
+                  modifiedGmt
+                }
+              }
+            }
+          }
+        `,
+          resolveSiteUrl: () => siteUrl,
+          resolvePages: ({
+            allSitePage: { nodes: allPages },
+            allWpContentNode: { nodes: allWpNodes },
+          }) => {
+            const wpNodeMap = allWpNodes.reduce((acc, node) => {
+              const { uri } = node
+              acc[uri] = node
+  
+              return acc
+            }, {})
+  
+            return allPages.map(page => {
+              return { ...page, ...wpNodeMap[page.path] }
+            })
+          },
+          serialize: ({ path, modifiedGmt }) => {
+            return {
+              url: path,
+              lastmod: modifiedGmt,
+            }
+          },
+        },
+      },
       {
         resolve: 'gatsby-plugin-robots-txt',
         options: {
