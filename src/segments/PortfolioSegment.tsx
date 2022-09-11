74674dda6411b3c29ba6f854672dd8d8dd155766
danieldb <danielduartebaird@gmail.com>
2022-09-10 23:32:00 -0400

    Fix portfolio iframe

--- src/segments/PortfolioSegment.tsx
+++ src/segments/PortfolioSegment.tsx
@@ -5,16 +5,16 @@ import isBrowser from "../helpers/isBrowser";
 function PortfolioSegment() {
   return (
     <TextBox title={"Our (old) Portfolio"}>
-      {/* <iframe
-        src="https://github.com/wheelerrobotics/wheelerrobotics.github.io/blob/master/Engineering%20Portfolio%202021-2022.pdf"
+      <iframe
+        src="https://wheelerrobotics.github.io/Engineering%20Portfolio%202021-2022.pdf"
         height={isBrowser() ? (window.innerWidth > 600 ? 900 : 500) : ""}
         width={isBrowser() ? (window.innerWidth > 600 ? 750 : 400) : ""}
         style={{ overflow: "hidden" }}
         frameBorder="0"
       >
         Loading…
-      </iframe> */}
-      Broken right now (will be fixed when i feel like it)
+      </iframe>
+      {/* Broken right now (will be fixed when i feel like it) */}
     </TextBox>
   );
 }
